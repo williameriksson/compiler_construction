4a9124545b70b284703f0982e5fe93a67ad66ea2
William Eriksson <willeeh@hotmail.com>
2016-09-14 19:53:37 +0200

    Removed unnessesary files

--- cimp/ocaml/_build/extract/listmap__Const.ml
+++
@@ -1,9 +0,0 @@
-(* This file has been generated from Why3 module Const *)
-
-open Why3extract
-
-let const (e: 'v) =
-  { Listmap__ListMap.view = (); Listmap__ListMap.c = e;
-   Listmap__ListMap.kl = []; Listmap__ListMap.vl = [] }
-
-
